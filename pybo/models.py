3c8ab1d9c10219eb19fb5cbf9c181536ebfb41c7
jssoft.ted <jssoft.ted@gmail.com>
2025-09-30 11:16:10 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
+    modify_date = models.DateTimeField(null=True, blank=True)
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    voter = models.ManyToManyField(User, related_name='voter_question')  # 추천인 추가
+    
+    def __str__(self):
+        return self.subject
+
+
+class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    voter = models.ManyToManyField(User, related_name='voter_answer')
